cfd3ec86e500d4925a984419862780e199a5fe68
pkaiserui <peterkaiserui@gmail.com>
2015-04-21 11:56:23 -0700

    added tableview

--- Fish Bowl/Fish Bowl/AddWordsViewController.swift
+++ Fish Bowl/Fish Bowl/AddWordsViewController.swift
@@ -8,11 +8,35 @@
 
 import UIKit
 
-class AddWordsViewController: UIViewController{
+class AddWordsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    
+    var superheroes = [String]()
+    
+    let path = NSBundle.mainBundle().pathForResource("questions", ofType: "plist")
     
     
     
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return superheroes.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("questionCell")as! UITableViewCell
+        
+        cell.textLabel?.text = superheroes[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    override func viewDidLoad() {
+        if let path = path {
+            superheroes = NSArray(contentsOfFile: path) as! [String]
+        }
+    }
     
     
     
